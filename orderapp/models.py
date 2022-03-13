dd574b56c940058ff54d3051a02791b5bdcaab80
mohammad <mr.dori.dev@gmail.com>
2022-03-13 11:48:40 +0330

    create customer, product and orderapp models

--- orderapp/models.py
+++ orderapp/models.py
@@ -1,3 +1,38 @@
+"""models"""
+from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class Customer(models.Model):
+    """costaomer models"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.FileField(
+        upload_to="files/user_avatar", null=False, blank=False)
+    description = models.CharField(max_length=512, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+
+
+class Product(models.Model):
+    """product models"""
+    title = models.CharField(max_length=50, null=False, blank=False)
+    cover = models.FileField(
+        upload_to="files/product_cover", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class OrderApp(models.Model):
+    """order models"""
+    title = models.CharField(max_length=50, null=False, blank=False)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    create_at = models.DateTimeField(default=datetime.now())
+    description = RichTextField()
+
+    def __str__(self):
+        return f"{self.title} {self.customer} {self.product}"
